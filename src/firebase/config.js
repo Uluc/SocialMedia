092a2558d683e75dafaf43bd36f1252fff9e1f41
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-10-07 12:39:44 -0500

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage'; // 
+import 'firebase/firestore'; //database
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAhgewb3SvuxsqyoyucbiajryhcOVL3GKY",
+    authDomain: "photography-website-2adf4.firebaseapp.com",
+    databaseURL: "https://photography-website-2adf4.firebaseio.com",
+    projectId: "photography-website-2adf4",
+    storageBucket: "photography-website-2adf4.appspot.com",
+    messagingSenderId: "377273324266",
+    appId: "1:377273324266:web:67edc6dad6e805549dfd93"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage(); //interact with storage
+const projectFirestore = firebase.firestore(); //interact with database
+
+export {
+    projectFirestore,
+    projectStorage
+};
